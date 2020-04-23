c17b5c78b6f09144b3e4b183c9c6688fb68e408f
rsanchez1993 <60765802+rsanchez1993@users.noreply.github.com>
2020-04-22 20:43:24 -0700

    Add files via upload

---
+++ processStatisticUpdate.php
@@ -0,0 +1,22 @@
+<?php
+
+// create short variable names
+$name       = preg_replace("/\t|\R/",' ',$_POST['name']);
+$time       = preg_replace("/\t|\R/",' ',$_POST['time']);
+$points     = (int) $_POST['points'];
+$assists    = (int) $_POST['assists'];
+$rebounds   = (int) $_POST['rebounds'];
+
+
+require('PlayerStatistic.php');
+
+$newStat = new PlayerStatistic($name, $time, $points, $assists, $rebounds);
+
+if( ! empty($name) )
+{
+  file_put_contents('../data/statistics.txt', $newStat->toTSV()."\n", FILE_APPEND | LOCK_EX);
+}
+
+require('home_page.php');
+?>
+
